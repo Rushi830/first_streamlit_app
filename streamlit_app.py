eae39bca2366ff46bf3c01d668ef840154446bd6
Rushi830 <89636518+Rushi830@users.noreply.github.com>
2022-08-02 23:00:50 +0530

    Update streamlit_app.py
    ttt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-import requests
+#import requests
 import pandas
 from urllib.error import URLError
 streamlit.title('app testing check')
@@ -22,16 +22,19 @@ streamlit. dataframe (fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-add_my_fruit = streamlit.text_input('What second fruit would you like information about?','apple')
-streamlit.write('The user entered ', add_my_fruit)
-streamlit.write('Thanks for adding my fruit ', add_my_fruit)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+#streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+excepr URLError as e:
+  streamlit.error()
 #streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -43,3 +46,7 @@ my_cur.execute("Insert into fruit_load_list values('from streamlit')")
 streamlit.stop()
 
 
+
+#add_my_fruit = streamlit.text_input('What second fruit would you like information about?')
+#streamlit.write('The user entered ', add_my_fruit)
+#streamlit.write('Thanks for adding my fruit ', add_my_fruit)
