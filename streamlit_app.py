db5e89dab1ab886760491ecd9b0bcdd1abb30f18
Rushi830 <89636518+Rushi830@users.noreply.github.com>
2022-08-02 22:49:29 +0530

    Update streamlit_app.py
    ffff

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruit_choice2 = streamlit.text_input('What second fruit would you like information about?','apple')
-streamlit.write('The user entered ', fruit_choice2)
+add_my_fruit = streamlit.text_input('What second fruit would you like information about?','apple')
+streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding my fruit ', add_my_fruit)
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -37,7 +38,7 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_row)
-
+my_cur.execute("Insert into fruit_load_list values('from streamlit')")
 
 
 
